861198ab0a69808197d011ed7092ce666da29834
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2021-08-04 23:42:18 +0900

    Update Auto response.js

--- Auto response.js
+++ Auto response.js
@@ -6,19 +6,32 @@ client.on("ready", () => {
     }
 );
 
-client.on(
-    "message", async msg => {
-	    if (msg.author.bot) return;
+client.on("message", async message => {
+	    //if (msg.author.bot) return;
 	
 	    switch (msg.content) {
 		    case "테스트":
-		        msg.reply("테스트 성공!"); //@건우, 테스트 성공!
+		        msg.reply("테스트 성공!");
+		        message.channel.send("테스트 성공!");
 		        break;
 		    case "ping":
-		        msg.reply("ping: ${client.ws.ping}ms");
+		        msg.reply(`ping: ${client.ws.ping}`);
+		        break;
+		    case "리로드":
+		        msg.reply("cd discord-bot\nnode app");
 		        break;
 		}
-    }
-);
+    
 
-client.login(''); token
+
+if (message.content == "임베드") {
+	const embed = new Discord.MessageEmbed()
+	    .setTitle("Test")
+	    .setDescription("Content")
+	    .setColor("#00ffff")
+	    .setFooter(`Request: ${message.author.username}`)
+	message.channel.send(embed)
+}
+}
+);
+client.login('');
